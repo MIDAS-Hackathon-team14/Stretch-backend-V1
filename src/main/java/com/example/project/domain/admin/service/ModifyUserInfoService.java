354f2d1e4d06e64a5c4d02771c2487b221796499
rlaisqls <rlaisqls@gmail.com>
2022-11-03 21:33:47 +0900

    Feat: 유저 정보 수정 기능

--- src/main/java/com/example/project/domain/admin/service/ModifyUserInfoService.java
+++ src/main/java/com/example/project/domain/admin/service/ModifyUserInfoService.java
@@ -1,4 +1,23 @@
 package com.example.project.domain.admin.service;
 
+import com.example.project.domain.admin.presentation.dto.request.ModifyUserInfoRequest;
+import com.example.project.domain.user.domain.User;
+import com.example.project.domain.user.domain.repository.UserRepository;
+import com.example.project.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ModifyUserInfoService {
+
+    private final UserFacade userFacade;
+    private final UserRepository userRepository;
+
+    public void execute(Long userId, ModifyUserInfoRequest request) {
+
+        User user = userFacade.getUserById(userId);
+        user.modifyInfo(request.getName(), request.getEmail());
+
+    }
 }
